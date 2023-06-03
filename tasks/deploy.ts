fd2e126b901d8112be492769e00c006690adac91
Thor Seldon <thorseldon@protonmail.com>
2023-06-03 15:49:10 +0800

    chore: Upgrading the nft vault for paired nfts

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -359,10 +359,22 @@ task("deploy:NewImpl", "Deploy new implmentation")
     await run("set-DRE");
     await run("compile");
 
-    await deployImplementation(implid, false);
+    await deployImplementation(implid, true);
 
     console.log("ok");
   });
+
+task("deploy:NewImpl:NftVault", "Deploy new implmentation").setAction(async (_, { run }) => {
+  await run("set-DRE");
+  await run("compile");
+
+  await deployContract("VaultLogic", [], true);
+  const vaultLogic = await getContractAddressFromDB("VaultLogic");
+
+  const vaultImpl = await deployImplementation("NftVault", true, { VaultLogic: vaultLogic });
+
+  console.log("Implmentation at:", vaultImpl.address);
+});
 
 task("prepareUpgrade", "Deploy new implmentation for upgrade")
   .addParam("proxyid", "The proxy contract id")
